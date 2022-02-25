850080103a4b78496e17ec70eb12e63b8bcc9cbc
De La Riva <delariva8964@gmail.com>
2022-02-25 18:34:31 +0800

    fix: use img instead of Iamge due to export not support

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -1,14 +1,12 @@
 import { FC } from "react";
-import Image from 'next/image'
 import LogoPng from '../../public/app-logo.svg'
-import { useSigningCosmWasmClient } from "../../hooks";
 
 const Navbar: FC = () => {
     return (
         <div className="relative pb-16">
             <nav className="fixed z-50 t-0 w-full bg-white border-gray-100 border-b px-2 sm:px-4 py-2.5 dark:bg-gray-800">
                 <div className="container mx-auto flex items-center justify-center">
-                    <Image src={LogoPng} alt="depub.Page" width="220" height="45"/>
+                    <img src={LogoPng} alt="depub.Page" width="220" height="45"/>
                 </div>
             </nav>
         </div>
--- components/User/Profile.tsx
+++ components/User/Profile.tsx
@@ -5,12 +5,6 @@ const UserProfile = (props: any) => {
     return (
         <div className="flex items-center space-x-4">
             <div className="flex-shrink-0">
-                {/* <Image 
-                    className="w-8 h-8 rounded-full"
-                    src={testImage}
-                    width="40"
-                    height="40"
-                    alt=""/> */}
             </div>
             <div className="flex-1 min-w-0">
                 <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
